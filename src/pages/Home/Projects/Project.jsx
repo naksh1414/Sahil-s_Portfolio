69e2a95e3817b8b9b8b277e3a559631eee9f3ce4
Nakshatra Manglik <nakshatramanglik14@gmail.com>
2024-06-14 22:14:10 +0530

    responsive Portfolio

--- src/pages/Home/Projects/Project.jsx
+++ src/pages/Home/Projects/Project.jsx
@@ -1,5 +1,6 @@
 // import MediaControlCard from "../../components/Card/ProjectCard";
 // import img from "../../assets/work/image 1.svg";
+import { Link } from "react-router-dom";
 import img1 from "../../../assets/Projects/Rectangle 16.svg";
 import img2 from "../../../assets/Projects/Rectangle 17.svg";
 import img3 from "../../../assets/Projects/Rectangle 18.svg";
@@ -16,28 +17,28 @@ const Project = () => {
       </div>
       <div className="flex flex-col justify-center mt-10 p-4">
         <div className="md:flex justify-center space-x-9">
-          <a href="/barber_project">
+          <Link to="/barber_project">
             <img src={img1} alt="image" />
-          </a>
-          <a href="/atrangi_project">
+          </Link>
+          <Link to="/atrangi_project">
             <img className="md:pt-[100px]" src={img2} alt="image" />
-          </a>
+          </Link>
         </div>
         <div className="md:flex justify-center space-x-10">
-          <a href="/broker_project">
+          <Link to="/broker_project">
             <img className=" mt-10 md:mt-[-150px]" src={img3} alt="image" />
-          </a>
-          <a href="/gdsc_project">
+          </Link>
+          <Link to="/gdsc_project">
             <img className="pt-[60px]" src={img4} alt="image" />
-          </a>
+          </Link>
         </div>
       </div>
       <div className="flex justify-center items-center mt-14  ">
-        <a href="/explore">
+        <Link to="/explore">
           <button className="bg-red-600 w-[300px] text-white py-3 px-6 rounded-md">
             EXPLORE MORE PROJECTS
           </button>
-        </a>
+        </Link>
       </div>
     </main>
   );
